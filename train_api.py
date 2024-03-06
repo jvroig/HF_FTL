3c27146b9b7da2bfae59578f36c414163aac10f0
JV Roig <jvroig@gmail.com>
2024-03-06 14:10:01 +0800

    Added api/config and js config retrieval

--- train_api.py
+++ train_api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from hf_local_config import *
 
 app = Flask(__name__)
 CORS(app)  # Add this line to enable CORS for all routes
@@ -11,5 +12,14 @@ def receive_message():
     response = {"message": f"I received your message. Your message was: {message}"}
     return jsonify(response)
 
+@app.route('/api/config', methods=['GET'])
+def get_config_paths():
+    config_paths = {
+        "model_path": model_path,
+        "finetuned_path": finetuned_path
+    }
+    return jsonify(config_paths)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
